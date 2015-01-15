47b7e72a5fb61bf8e2d6bfe42b4dbeadbb2fe8d1
chutch01 <chutch01@hotmail.com>
2015-01-15 15:45:45 -0500

    updated Hero Class

--- COMP1101-Lesson2/src/Hero.java
+++ COMP1101-Lesson2/src/Hero.java
@@ -30,15 +30,26 @@ public class Hero {
 	}
 	
 	private void generateAbilities(){
+		this.strength = (int)(Math.random() * 100 +1);
+		this.speed = (int)(Math.random() * 100 +1);
+		this.health = (int)(Math.random() * 100 +1);
+	}
+	//public methods ++++++++++++++++++++++++++++++++++++++++++++++++++
+	public void showAbilities(){
+		System.out.println("+++++++++++++++++++");
+		System.out.println("Strength: " + this.strength);
+		System.out.println("Speed: " + this.speed);
+		System.out.println("Health: " + this.health);
+		System.out.println("+++++++++++++++++++");
 		
 	}
 	
 	public void fight(){
-		
+		System.out.println(this.name + " is fighting!");
 	}
 	
 	public void run(){
-		
+		System.out.println(this.name + " is running!");	
 	}
 
 
