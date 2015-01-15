0f78351aebf544e2f27c3410daf979381c62cffe
chutch01 <chutch01@hotmail.com>
2015-01-15 16:16:04 -0500

    added villan class and updated the project

--- COMP1101-Lesson2/src/Project.java
+++ COMP1101-Lesson2/src/Project.java
@@ -3,7 +3,15 @@ public class Project {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Hero bigMan = new Hero ("Big Man");
+		bigMan.fight();
+		bigMan.run();
+		bigMan.showAbilities();
+		Villan villan = new Villan("Sly Man");
+		villan.run();
+		villan.steals();
+		villan.showAbilities();
+		
 	}
 
 }
---
+++ COMP1101-Lesson2/src/Villan.java
@@ -0,0 +1,14 @@
+
+public class Villan extends Hero {
+
+	public Villan(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	//public methods++++++++++++++++++++++++++++++++++++++
+	public void steals(){
+		System.out.println(this.name + " stole a thing!");
+		
+		
+	}
+}
